295a161c6d610309e7b1a879ef82cce6795b8e78
Gabriel Arruda <gabriel.arrudar@gmail.com>
2025-04-16 19:03:18 +0000

    feat: create uuid value object and unit test

---
+++ src/shared/domain/value-objects/uuid.value-object.spec.ts
@@ -0,0 +1,18 @@
+import { InvalidUuidError, Uuid } from "./uuid.value-object";
+import { validate as uuidValidate } from "uuid";
+
+describe("Uuid Unit Tests", () => {
+  const validateSpy = jest.spyOn(Uuid.prototype as any, "validate");
+  it("should throw error when uuid is invalid", () => {
+    expect(() => {
+      new Uuid("invalid-uuid");
+    }).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should create a valid uuid when no id is passed", () => {
+    const uuid = new Uuid();
+    expect(uuidValidate(uuid.id)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  });
+});
