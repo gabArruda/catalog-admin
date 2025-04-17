000735c42bd962a7c599690ebb6d99a2bdda1c7c
Gabriel Arruda <gabriel.arrudar@gmail.com>
2025-04-17 13:54:53 +0000

    feat: make category entity validate itself

--- src/category/domain/category.entity.spec.ts
+++ src/category/domain/category.entity.spec.ts
@@ -1,7 +1,12 @@
+import { EntityValidationError } from "../../shared/domain/validators/validation.error";
 import { Uuid } from "../../shared/domain/value-objects/uuid.value-object";
 import { Category } from "./category.entity";
 
 describe("Category unit tests", () => {
+  let validateSpy: jest.SpyInstance<void, [entity: Category], any>;
+  beforeEach(() => {
+    validateSpy = jest.spyOn(Category, "validate");
+  });
   describe("constructor", () => {
     it("should create a category with default values", () => {
       const category = new Category({
@@ -43,6 +48,7 @@ describe("Category unit tests", () => {
       expect(category.is_active).toBeFalsy();
       expect(category.created_at).toBeInstanceOf(Date);
       expect(category.updated_at).toBeInstanceOf(Date);
+      expect(validateSpy).toHaveBeenCalledTimes(1);
     });
 
     it("should create a category with optional values", () => {
@@ -57,6 +63,7 @@ describe("Category unit tests", () => {
       expect(category.is_active).toBeTruthy();
       expect(category.created_at).toBeInstanceOf(Date);
       expect(category.updated_at).toBeInstanceOf(Date);
+      expect(validateSpy).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -67,6 +74,7 @@ describe("Category unit tests", () => {
       });
       category.changeName("TV Show");
       expect(category.name).toBe("TV Show");
+      expect(validateSpy).toHaveBeenCalledTimes(2);
     });
 
     it("should change category description", () => {
@@ -76,6 +84,7 @@ describe("Category unit tests", () => {
       expect(category.description).toBeNull();
       category.changeDescription("Movie description");
       expect(category.description).toBe("Movie description");
+      expect(validateSpy).toHaveBeenCalledTimes(2);
     });
 
     it("should activate and deactivate a category", () => {
@@ -87,6 +96,40 @@ describe("Category unit tests", () => {
       expect(category.is_active).toBeTruthy();
       category.deactivate();
       expect(category.is_active).toBeFalsy();
+    });
+  });
+});
+
+describe("Category validator tests", () => {
+  describe("create command", () => {
+    it("should throw when name is invalid", () => {
+      expect(() => {
+        Category.create({ name: "" });
+      }).toThrow(EntityValidationError);
+      expect(() => {
+        Category.create({ name: null as unknown as string });
+      }).toThrow(EntityValidationError);
+      expect(() => {
+        Category.create({ name: "a".repeat(256) });
+      }).toThrow(EntityValidationError);
+      expect(() => {
+        Category.create({ name: 10 as unknown as string });
+      }).toThrow(EntityValidationError);
+    });
+
+    it("should throw when description is invalid", () => {
+      expect(() => {
+        Category.create({ name: "test", description: 10 as unknown as string });
+      }).toThrow(EntityValidationError);
+    });
+
+    it("should throw when is_active is invalid", () => {
+      expect(() => {
+        Category.create({ name: "test", is_active: 10 as unknown as boolean });
+      }).toThrow(EntityValidationError);
+      expect(() => {
+        Category.create({ name: "test", is_active: "" as unknown as boolean });
+      }).toThrow(EntityValidationError);
     });
   });
 });
--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,4 +1,7 @@
+import { EntityValidationError } from "../../shared/domain/validators/validation.error";
+import { FieldsErrors } from "../../shared/domain/validators/validator-fields.interface";
 import { Uuid } from "../../shared/domain/value-objects/uuid.value-object";
+import { CategoryValidatorFactory } from "./category.validator";
 
 export type CategoryConstructorProps = {
   category_id?: Uuid;
@@ -33,15 +36,19 @@ export class Category {
   }
 
   static create(props: CategoryCreateCommand): Category {
-    return new Category(props);
+    const category = new Category(props);
+    Category.validate(category);
+    return category;
   }
 
   changeName(name: string): void {
     this.name = name;
+    Category.validate(this);
   }
 
   changeDescription(description: string): void {
     this.description = description;
+    Category.validate(this);
   }
 
   activate() {
@@ -50,6 +57,14 @@ export class Category {
 
   deactivate() {
     this.is_active = false;
+  }
+
+  static validate(entity: Category) {
+    const validator = CategoryValidatorFactory.create();
+    const isValid = validator.validate(entity);
+    if (!isValid) {
+      throw new EntityValidationError(validator.errors);
+    }
   }
 
   toJSON() {
